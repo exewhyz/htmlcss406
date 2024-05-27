b3f4e5a273b2dfbf3e993610eafa11e6a6e4997d
exewhyz <araj52444@gmail.com>
2024-05-27 16:44:55 +0530

    completed js

--- script.js
+++ script.js
@@ -131,8 +131,168 @@ let age = ["Nine", "Five", "Ten"];
 
 // console.log(sumValue);
 
-const arrowFunc = () => {
-  console.log("Arrow function");
+// const arrowFunc = () => {
+//   console.log("Arrow function");
+// };
+
+// arrowFunc();
+
+// Variable Hoisting
+// console.log(names);
+
+// var names;
+
+// let names; // not hoisted
+
+//function hoisting
+// hoisted();
+
+// function hoisted() {
+//   console.log("I am hoisted");
+// }
+
+//String
+
+const string1 = "Something1";
+
+const string2 = "Something2";
+
+const string3 = `Some ${string1} thing3 `;
+
+// const s4 = "I'm good"
+const s4 = " I'm good ";
+const s6 = ' I"m good ';
+const s7 = ` I'm good " `;
+
+//escape sequence characters(\', \n, \r, \t, \/, \b, \\, \v, \f, \")
+// const s8 = ' I\'m good ';
+// const s9 = ' I\nm good ';
+// const s10 = ' I\tm good ';
+// const s11 = ' I\rm good ';
+
+// console.log(s11);
+
+const str1 = "Something here";
+const str2 = "String2";
+const str3 = "SOMETHING CAPITAL";
+const str4 = "something small";
+
+// console.log(str1.split(" "));
+
+// console.log(str1.split(""));
+// console.log(str3.toLowerCase());
+// console.log(str4.toUpperCase());
+
+// console.log(str2.slice());
+// console.log(str2.slice(0,3));
+// console.log(str2.slice(2,4));
+// console.log(str2.slice(3));
+
+// console.log(str1.includes("here1"));
+
+// console.log(str2.indexOf("n"));
+
+function callbackFunc(a) {
+  console.log(a);
+}
+function normalFunc(a, callbackFunc) {
+  callbackFunc(a);
+}
+
+// normalFunc(100, callbackFunc);
+
+//Array
+
+// const arr1 = [];
+// console.log(arr1);
+const arr2 = ["one", "two", "three", "four"];
+const arr3 = [1, 2, 3, 4];
+
+// arr1[0] = 1;
+// arr1[1] = 2;
+// console.log(arr1)
+
+// arr1[1] = 5;
+
+// console.log(arr1);
+
+// console.log(arr2.length)
+
+// console.log(arr2.pop())
+
+// console.log(arr2)
+// arr2.push("five");
+// arr2.shift();
+// arr2.unshift("zero");
+// arr2.reverse();
+// const newString = arr3.toString();
+// const newArr = arr2.slice(0,3);
+// const joinedArr = arr2.join("-")
+// const joinedArr2 = arr2.join(arr3)
+// const mergedArrs = arr2.concat(arr3)
+
+// console.log(newString);
+// console.log(newArr);
+// console.log(joinedArr);
+// console.log(joinedArr2);
+// console.log(mergedArrs);
+
+// const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// const mapMethod = array.map((num) => {
+//   return num * 2;
+// });
+// console.log(mapMethod);
+
+// const filteredArray = array.filter((num, i) => {
+//   return num > 6;
+// });
+
+// console.log(filteredArray);
+
+// const reduceMethod = array.reduce((prev, curr) => {
+//   return prev + curr;
+// });
+
+// console.log(reduceMethod);
+
+// let a = 10;
+// let b = a;
+
+// a = 20;
+
+// console.log("a", a);
+// console.log("b", b);
+
+let obj1 = {
+  name: "Aniket",
+  phone: "123",
+  add: {
+    city: "Pune",
+    state: "Maharashtra",
+    country: "India",
+  },
 };
 
-arrowFunc();
+// let obj2 = { ...obj1 }; //shallow cloning or copy
+// let obj2 = { ...obj1, add: { ...obj1.add } }; //deep cloning or copy
+
+// const stringifiedObj1 = JSON.stringify(obj1);
+
+// const parsedObj1 = JSON.parse(stringifiedObj1);
+// let obj2 = parsedObj1;
+
+// let obj2 = obj1;
+
+// console.log("Prev obj1", obj1);
+// console.log("Prev obj2", obj2);
+
+// obj1.name = "John";
+
+// obj2.name = "John";
+// obj2.add.city = "Mumbai";
+
+// console.log("obj1", obj1);
+// console.log("obj2", obj2);
+// console.log(obj1 === obj2);
+
